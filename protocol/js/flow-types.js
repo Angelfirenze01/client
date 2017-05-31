7115de939d07e220e63af78d71b4d09d961bfdb6
Miles Steele <miles@milessteele.com>
2017-05-31 13:59:22 -0400

    TEMP
    proto2
    
    take a break to factor out basebox
    
    TEMP
    
    TEMP
    
    TEMP
    
    TEMP
    
    TEMP
    
    crazy: make Team wrapper for TeamData
    
    TEMP
    
    TEMP
    
    fix member test (compiles, fails)
    
    move keymanager around
    
    attach to G
    
    TEMP
    
    test

--- protocol/js/flow-types.js
+++ protocol/js/flow-types.js
@@ -4992,6 +4992,14 @@ export type PerTeamKey = {
   encKID: KID,
 }
 
+export type PerTeamKeySeed = any
+
+export type PerTeamKeySeedItem = {
+  seed: PerTeamKeySeed,
+  generation: int,
+  seqno: Seqno,
+}
+
 export type PerUserKey = {
   gen: int,
   seqno: Seqno,
@@ -5722,6 +5730,12 @@ export type TeamChangeReq = {
   writers?: ?Array<string>,
   readers?: ?Array<string>,
   none?: ?Array<string>,
+}
+
+export type TeamData = {
+  chain: TeamSigChainState,
+  perTeamKeySeeds?: ?Array<PerTeamKeySeedItem>,
+  readerKeyMasks?: ?Array<ReaderKeyMask>,
 }
 
 export type TeamID = string
